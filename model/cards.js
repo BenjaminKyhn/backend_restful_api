347a7f2f3ce6c581fcb33ba22d28f970811615e3
Benjamin Kyhn <benkyhn@hotmail.com>
2020-10-24 17:56:03 +0200

    Initial commit

---
+++ model/cards.js
@@ -0,0 +1,28 @@
+let cards = [
+    {
+        name: "Cryptic Command",
+        colour: "Blue",
+        manacost: "3U1",
+        type: "Instant",
+        image: "https://mythicspoiler.com/mdm/cards/crypticcommand.jpg",
+        inventory: 1,
+    },
+    {
+        name: "Faithless Looting",
+        colour: "Red",
+        manacost: "1R",
+        type: "Sorcery",
+        image: "https://mythicspoiler.com/ema/cards/faithlesslooting.jpg",
+        inventory: 2,
+    },
+    {
+        name: "Craterhoof Behemoth",
+        colour: "Green",
+        manacost: "3G5",
+        type: "Creature",
+        image: "https://mythicspoiler.com/mm3/cards/craterhoofbehemoth.jpg",
+        inventory: 1,
+    },
+]
+
+module.exports = cards;
---
+++ routes/cards.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var cards = require('../model/cards')
+
+/* GET cards */
+router.get('/', function(req, res, next) {
+    console.log("GET request received.")
+    res.send(JSON.stringify(cards));
+});
+
+router.get('/random', function(req, res, next) {
+    let randomCard = Math.floor(Math.random()*cards.length);
+    res.send(JSON.stringify(cards[randomCard].image));
+});
+
+router.put('/', function(req, res, next) {
+    obj = {}
+    obj.svar = "PUT request modtaget, data er opdateret.";
+    res.send(JSON.stringify(obj));
+});
+module.exports = router;
